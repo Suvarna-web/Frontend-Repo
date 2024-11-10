c46f685d0821a219c45d50ce2476f93dfca58225
Suvarna-web <suvarnasawant2003@gmail.com>
2024-11-10 13:41:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+//var a= Number (prompt("Enter your Number:"));
+//console.log(a)
+
+
+//let isDone =confirm ("do you task is complete")
+//console.log(isDone)
+
+
+//let notification =alert("you Have 14 Unread Message")
+//console.log(notification)
+
+var a ="Hello";
+console.log(Number(a))
+console.log(typeof NaN)
+
+var b ="23"
+console.log(Number(b))
+
+var c ="ajay"
+console.log(Boolean(c))
+
+var d =""
+console.log(Boolean(d))
+
+var n =45
+console.log(String(n))
+
+var Num = NaN;
+console.log(String(Num))
+
+var k = 0;
+console.log(Boolean(k))
+
+var j = 5;
+console.log(Boolean(j))
+
+var f = true;
+console.log(String(f))
+console.log(Number(f))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,14 @@
+
+
+var a=3;
+var b=5;
+var c=4;
+var d=3;
+
+
+console .log(a)
+console .log(b)
+console .log(c)
+console .log(d)
+
+a="hello";
