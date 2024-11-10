4922efd36ad01bdf62a266617a5e98c1c604316c
Suvarna-web <suvarnasawant2003@gmail.com>
2024-11-10 13:52:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-let divElem =document.getElementsByTagName("div")
-console.log(divElem);
+let li2= document.querySelector("hero");
+let mylist = document.querySelector(".list");
+let mycontainer = document.querySelector(".container");
 
+let newElem=document.createElement("li");
+let text="this is demo text 5";
+newElem.innerText = text;
+mylist.appendChild(newElem);
 
-let pElem =document.getElementsByTagName("p")
-console.log(pElem);
+let newElem2=document.createElement("li");
+let text2="this is demo text 6";
+newElem2.innerText = text2;
+mylist.appendChild(newElem2);
 
-let HeroElem =document.getElementsById("hero")
-console.log(HeroElem);
+let newElem3=document.createElement("li");
+let text3="this is demo text 0";
+newElem3.textContent = text3;
 
-let allBoxes =document.getElementsByClassName("box")
-console.log(allBoxes);
+mylist.insertBefore(newElem3, mylist.firstElementChild);
 
- // let anyElem =document.querySelector("div");
-//  let anyElem =document.querySelector("p");
-//  let anyElem =document.querySelector("#hero");
-let anyElem =document.querySelector(".box");
-let anyElems =document.querySelectorAll(".box");
+let newElem4=document.createElement("li");
+let text4="this is demo text 2.5";
+newElem4.innerText = text4;
+mylist.insertBefore(newElem4,li2.nextElementSibling);
 
-console.log(anyElem);
-console.log(anyElems);
+let items=mylist.children;
+let newElem5=document.createElement("li");
+let text5="this is demo text 4,5";
+newElem5.textContent = text5;
 
+//--------------------------------
 
+let p1=document.createElement("p");
+let p2=document.createElement("p");
+let p3=document.createElement("p");
+let p4=document.createElement("p");
+p1.textContent="this a p1";
+p2.textContent="this a p2";
+p3.textContent="this a p3";
+p4.textContent="this a p4";
 
+mylist.append(p1);
+mylist.prepend(p2);
+mylist.before(p3);
+mylist.after(p4);
 
