bdf8e5b6e09327f10de3258537817cd1d022c8ee
Suvarna-web <suvarnasawant2003@gmail.com>
2024-11-10 13:51:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,25 @@
-// Ex.1
-// console.log("Object: Player");
+let divElem =document.getElementsByTagName("div")
+console.log(divElem);
 
-// let Player = {
-//   name: "Prakash",
-//   age: 25,
-//   noOfMadels: 11,
-//   level: "National",
-// };
 
-// console.log(Player);
-// console.log(Player.name);
-// console.log(Player.level);
-// console.log(Player.age);
-// Player.age = 30;
-// ---------------------------------------
-// method 1
-let u1 = { name: "Prakash", age: 25 };
-let u2 = { name: "Rakesh", age: 30 };
-let u3 = { name: "Rajesh", age: 35 };
+let pElem =document.getElementsByTagName("p")
+console.log(pElem);
 
-var users = [u1, u2, u3];
+let HeroElem =document.getElementsById("hero")
+console.log(HeroElem);
 
-// // method 2
-var user = [
-  { name: "Prakash", age: 25 }, //0
-  { name: "Rakesh", age: 30 }, //1
-  { name: "Rajesh", age: 35 }, //2
-  { name: "Ramesh", age: 65 },
-  { name: "Ranjeet", age: 85 },
-  { name: "jeet", age: 25 },
-  { name: "Abhijeet", age: 51 },
-  { name: "Sujeet", age: 35 }, //n-1
-];
-//n = user.lenght
-// for (let i = 0; i < user.length; i++) {
-//   console.log("User " + (i + 1) + " : " + user[i].name);
-// }
-// using for of loop:
-for (let u of user) {
-  console.log(u.name + "'s age : " + u.age);
-}
-// Ex.2 
-// animal
-// Ex.3
-// Electronic_Devices
-// Ex.4
-// loctions
-// Ex.5
-// subject
+let allBoxes =document.getElementsByClassName("box")
+console.log(allBoxes);
+
+ // let anyElem =document.querySelector("div");
+//  let anyElem =document.querySelector("p");
+//  let anyElem =document.querySelector("#hero");
+let anyElem =document.querySelector(".box");
+let anyElems =document.querySelectorAll(".box");
+
+console.log(anyElem);
+console.log(anyElems);
+
+
+
+
