a94577035d1c87e429589823d8c913f6cd1206b4
Suvarna-web <suvarnasawant2003@gmail.com>
2024-12-04 13:58:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-console.log("Line a executed");
-console.log("Line b executed");
-console.log("Line c executed");
-console.log("Line d executed");
+let info;
+setTimeout(function () {
+  ifn = { name: "Raju" };
+}, 2000);
+
+console.log(info);
+
+// ------------------
+
+let data = new Promise((resolve, reject) => {
+  if (false) {
+    resolve({ name: "Raju" });
+  } else {
+    reject("Data not found");
+  }
+});
+
+data
+  .then(() => {
+    console.log(data);
+    return data;
+  })
+  .then((value) => {
+    console.log(value);
+    return value;
+  })
+  .then((value) => {
+    console.log(value.name);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
