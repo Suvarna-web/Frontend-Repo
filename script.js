46d38449348742a4a0344637d6409df635ea685e
Suvarna-web <suvarnasawant2003@gmail.com>
2024-12-11 10:22:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,13 @@
 
 console.log("hello");
 
-// windows location object  in js 
+console.log(history);
 
-//http--port--domain--extension --//--route--:--parems--?--queries
+console.log(window);
 
-//commands in commands prompt --
-//1.windows ip configure
-//2.ping www.google.com
-//3.tracert  route to www.google.com
+console.log(navigator);
 
-console.log(window)
-console.log(window.location)
-console.log(location.href)
+console.log(window.screen);
 
-function onHit(){
-    location.reload();
 
-}
-
-function ChangeUrl(){
-    location.replace("https://www.google.com");
-    
-}
-
-function ChangeUrlWithHistory(){
-    location.assign("https://www.google.com");
-    
-}
 
